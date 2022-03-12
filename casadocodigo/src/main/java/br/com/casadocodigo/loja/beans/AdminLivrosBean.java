8143ad75bd9b28e5cc74dd7ac46ab7cc113566ba
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-03-11 23:33:47 -0300

    jsf

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -1,12 +1,26 @@
 package br.com.casadocodigo.loja.beans;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
 import br.com.casadocodigo.loja.models.Livro;
 
+@Named
+@RequestScoped
 public class AdminLivrosBean {
 	
 	private Livro livro;
 
 	public void salvar() {
-		System.out.println("Livro salvo com sucesso");
+		System.out.println("Livro Cadastrado"+livro);
+		
+	}
+	
+	public Livro getLivro() {
+		return livro;
+	}
+	
+	public void setLivro(Livro livro) {
+		this.livro = livro;
 	}
 }
