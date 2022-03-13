9f9901d2f871ae547bfff35ecac7371e7b45a6d2
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-03-12 22:02:35 -0300

    JSF

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/LivroDao.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/LivroDao.java
@@ -1,16 +1,20 @@
 package br.com.casadocodigo.loja.daos;
 
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import br.com.casadocodigo.loja.models.Livro;
+
 
 public class LivroDao {
 
 	@PersistenceContext
 	private EntityManager manager;
 	
-	public void salva(Livro livro) {
+	public void salvar(Livro livro) {
+		
 		manager.persist(livro);
+	
 	}
 }
