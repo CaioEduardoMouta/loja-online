4a5b1d015442d7094d462a759d55a347fc2236a3
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-03-18 23:34:18 -0300

    java

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/infra/FileSaver.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/infra/FileSaver.java
@@ -1,8 +1,18 @@
 package br.com.casadocodigo.loja.infra;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.Part;
+import javax.validation.Path;
+import java.io.File;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+import java.nio.file.Paths;
 
 public class FileSaver {
 	
@@ -10,15 +20,34 @@ public class FileSaver {
 
 	public String write(Part arquivo, String path) throws IOException {
 		String relativePath = path + "/" + arquivo.getSubmittedFileName();
-		
+		String fullPath = SERVER_PATH + "/" +relativePath;
+
 		try {
-			arquivo.write(SERVER_PATH + "/" +relativePath);
-			return relativePath;
+			arquivo.write(fullPath);
 		}catch (IOException e) {
 			throw new RuntimeException(e);
 		}
+		
+		return relativePath;
 	}
-
-
+	
+	public static void transfer(Path source, ServletOutputStream outputStream) {
+		try {
+			  FileInputStream input = new FileInputStream(source.toFile());
+		try(ReadableByteChannel inputChannel = Channels.newChannel(input);
+				WritableByteChannel outputChannel = Channels.newChannel(outputStream)){
+			ByteBuffer buffer = ByteBuffer.allocateDirect(1024 * 10);
+			
+			while(inputChannel.read(buffer) != - 1) {
+				outputChannel.write(buffer);
+				buffer.clear();
+			}
+		}catch(IOException e) {
+			throw new RuntimeException(e);
+		}
+	}catch (FileNotFoundException e) {
+		throw new RuntimeException(e);
+	}
+	}
 	
 }
